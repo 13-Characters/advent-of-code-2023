29f610ee64f059ae435fc9f7cb752a6411e924d5
13Characters_ <13.0character@gmail.com>
2023-12-02 21:52:48 -0800

    Completed Part 1

--- day3/part1.py
+++ day3/part1.py
@@ -5,16 +5,39 @@ import numpy as np
 johns_matrix = open("input.txt").readlines() # variable's name is an old running joke
 # johnathans_matrix = np.ndarray((len(input[0].removesuffix("\n")), len(input)), np.character)
 
+number_positions = []
+numbers = []
+length = 0
 for y, row in enumerate(johns_matrix):
     for x, char in enumerate(row):
-        if char in string.punctuation and char != ".":
-            adjacents = []
-            for row2 in johns_matrix[y - 1:y + 2]:
-                for char2 in row[x - 1:x + 2]:
-                    adjacents.append(char2)
-            adjacents.pop(4)  # this is a bad way of getting adjacents
-            for adjacent in adjacents:
-                if adjacent in string.digits:
+        if char in string.digits:
+            if length == 0:
+                number_positions.append([x, y, length + 1]) # These are how all the digit positions are stored
+                numbers.append(int(char)) # These are how the numbers will be stored
+            else:
+                (number_positions[-1])[2] += 1
+                numbers[-1] = numbers[-1] * 10 + int(char)
+            length += 1
+        else: length = 0
 
+indicesToRemove = []
+for i in range(len(number_positions)):
+    number_position = number_positions[i]
+    number = numbers[i]
+    numberIsValid = False
+    for y in range(number_position[1]-1, number_position[1]+2):
+        for x in range(number_position[0]-1, number_position[0]+number_position[2]+1):
+            if (0 <= x < len(johns_matrix[0].removesuffix("\n")) and 0 <= y < len(johns_matrix)
+                    and johns_matrix[y][x] in string.punctuation and johns_matrix[y][x] != "."):
+                numberIsValid = True
+                break
+    if not numberIsValid:
+        indicesToRemove.append(i)
 
+for index in reversed(indicesToRemove):
+    number_positions.pop(index)
+    numbers.pop(index)
+
+print(numbers)
+print(sum(numbers))
 
