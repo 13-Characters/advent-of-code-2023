8092fc327242995bda10d2e2185c21745615c13b
13Characters_ <13.0character@gmail.com>
2023-12-03 23:16:21 -0800

    Completed Day 4

---
+++ day4/part1.py
@@ -0,0 +1,14 @@
+input = open("input.txt").readlines()
+result = 0
+for i, line in enumerate(input):
+    line = line.removeprefix(f"Card   {i+1}: ")
+    line = line.removesuffix(f"\n")
+    winning, card = line.split(" | ")
+    winning_numbers = set([w for w in winning.split(" ") if w != "" and w != " "]) # list comprehension is in so nothing weird happens
+    card_numbers = set([c for c in card.split(" ") if c != "" and c != " "])
+
+    if len(winning_numbers & card_numbers) > 0:
+        card_value = 1 << (len(winning_numbers & card_numbers) - 1)
+    else: card_value = 0
+    result += card_value
+print(result)
---
+++ day4/part2.py
@@ -0,0 +1,21 @@
+input = open("input.txt").readlines()
+result = 0
+def playCard(i):
+    cardsWon = 0
+    line = input[i]
+    line = line.removeprefix(f"Card   {i+1}: ")
+    line = line.removesuffix(f"\n")
+    winning, card = line.split(" | ")
+    winning_numbers = set([w for w in winning.split(" ") if w != "" and w != " "]) # list comprehension is in so nothing weird happens
+    card_numbers = set([c for c in card.split(" ") if c != "" and c != " "])
+    if len(winning_numbers & card_numbers) > 0:
+        for a in range(i+1, i + len(winning_numbers & card_numbers) + 1):
+            cardsWon += playCard(a)
+        cardsWon += 1
+    else: cardsWon = 1
+    return cardsWon
+
+for i in range(len(input)):
+    result += playCard(i)
+
+print(result)
