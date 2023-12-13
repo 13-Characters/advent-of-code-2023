7da48f043489fc96a2ac5fe2da58587b48d62d6c
13Characters_ <13.0character@gmail.com>
2023-12-13 11:30:51 -0800

    Finished Day 13 Part 1

--- day13/part1.py
+++ day13/part1.py
@@ -1,12 +1,50 @@
 import numpy as np
-def verticalReflectionTester(grid: list[str][str]):
+def reflectionTester(grid: list[list[str]]):
+    reflectionRow = -1
     # Find potential reflection line
-    for i, row in grid:
+    for i, row in enumerate(grid):
+        if row not in grid[i+1:]:
+            reflectionRowFound = False
+            continue
+        else:
+            j = -1
+            # Case 1: the reflection line is above the middle of the grid
+            if i == 0:
+                j = grid.index(row, i+1)
+            # Case 2: the reflection line is below the middle of the grid
+            if grid[-1] == row and i != 0:
+                j = len(grid) - 1
+            if j != -1:
+                reflectionRow = (i + j) // 2 + 1
+                # Confirm that this horizontal line is a valid reflection line
+                leftHalf = grid[:reflectionRow]
+                rightHalf = grid[reflectionRow:]
+                if len(leftHalf) > len(rightHalf):
+                    leftHalf = leftHalf[-len(rightHalf):]
+                if len(rightHalf) > len(leftHalf):
+                    rightHalf = rightHalf[:len(leftHalf)]
+                if leftHalf == list(reversed(rightHalf)):
+                    return reflectionRow
+    return -1
 
-input = open("example_input.txt").readlines()
+input = open("input.txt").readlines()
 parsedInput = [[]]
 for line in input:
     if line == "\n":
         parsedInput.append([])
     else:
-        parsedInput[-1].append(line.removesuffix("\n"))
+        parsedInput[-1].append(list(line.removesuffix("\n")))
+
+result = 0
+counter = 0
+for grid in parsedInput:
+    counter += 1
+    if reflectionTester(grid) != -1:
+        result += 100 * reflectionTester(grid)
+        print(counter, reflectionTester(grid), "horizontal")
+    else:
+        grid = [list(x) for x in np.array(grid).transpose()]
+        if reflectionTester(grid) != -1:
+            result += reflectionTester(grid)
+            print(counter, reflectionTester(grid), "vertical")
+print(result)
